ce111c4727c28ca889c16885f92312e845065b87
mcbtr <mcb.gitlab@gmail.com>
2023-09-27 12:26:05 +0200

    Añadimos una clase poara tener un textfield generico para la aplicacion

---
+++ lib/componentes/customTextField.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class customTextField extends StatelessWidget
+{
+
+  TextEditingController tecUsername = TextEditingController();
+  String contenido;
+
+  customTextField({Key? key, required this.contenido, required this.tecUsername}) : super (key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    Row row = Row(
+      children: [
+        Flexible(child : TextFormField(
+          decoration: InputDecoration(
+              border:  OutlineInputBorder(),
+              hintText: contenido
+          ),
+        )
+        )
+      ],
+    );
+    return row;
+  }
+
+
+
+
+
+}

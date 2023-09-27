0120cca81d8ed56ac5af87271b17247df3e63cf3
mcbtr <mcb.gitlab@gmail.com>
2023-09-27 12:04:37 +0200

    Guardado antes de iniciar el comando  Firebase.initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'Actividad1.dart';
 import 'firebase_options.dart';
 
 void main() async{
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   Actividad1 actividad1 = Actividad1();
   runApp(actividad1);
+
+
 }

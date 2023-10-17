8fc495ff01ebbdbc1896f675e772e62f46d18c32
Manuel <mcb.gitlab@gmail.com>
2023-10-17 10:41:46 +0200

    Se subio mal el commit

---
+++ lib/ClasesPropias/CustomCellView.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomCellView extends StatelessWidget{
+
+  final String sTexto;
+  final int iCodigoColor;
+  final double dFuenteTamanyo;
+
+  const CustomCellView({super.key,
+    required this.sTexto,
+    required this.iCodigoColor,
+    required this.dFuenteTamanyo});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+        color: Colors.amber[iCodigoColor],
+        child: Row(
+          children: [
+            Image.asset("resources/logo_kyty2.png",width: 30,
+                height: 45),
+            Text(sTexto)
+          ],
+        )
+
+    );
+  }
+
+
+
+}

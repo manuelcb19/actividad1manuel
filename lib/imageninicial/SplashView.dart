20adb4397297002205d4c34a60acb57ddd5411bb
Manuel <mcb.gitlab@gmail.com>
2023-10-17 10:40:19 +0200

    Se subio mal el commit

--- lib/imageninicial/SplashView.dart
+++ lib/imageninicial/SplashView.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../FbClases/CustomUsuario.dart';
+import '../ClasesPropias/CustomUsuario.dart';
 
 class SplashView extends StatefulWidget {
 

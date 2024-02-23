b512b8c1c5fa523729749a7606206ea9fed0f425
mcbtr <mcb.gitlab@gmail.com>
2024-02-23 11:35:09 +0100

    Se consigue realizar el segundo punto

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const { onRequest } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
 
 exports.insertElement = onRequest(async (request, response) => {
   try {
@@ -24,3 +25,29 @@ exports.insertElement = onRequest(async (request, response) => {
     response.status(500).send('Error al insertar el elemento en la base de datos.');
   }
 });
+
+exports.deleteElement = functions.https.onRequest(async (request, response) => {
+  try {
+
+    const elementId = request.query.elementId;
+
+
+    if (!elementId) {
+      return response.status(400).send('El parámetro elementId es obligatorio.');
+    }
+
+    await admin.firestore().collection('Usuarios').doc(elementId).delete();
+
+
+    functions.logger.info(`El elemento con ID ${elementId} se eliminó correctamente.`, { structuredData: true });
+
+    response.status(200).send(`El elemento con ID ${elementId} se eliminó correctamente.`);
+  } catch (error) {
+    console.error('Error al eliminar el elemento:', error);
+
+
+    functions.logger.error('Error al eliminar el elemento en Firestore.', { error: error });
+
+    response.status(500).send('Error al eliminar el elemento en Firestore.');
+  }
+})

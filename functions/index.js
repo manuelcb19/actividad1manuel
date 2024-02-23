e7110e4ab16e068c9a1ba6849da81239c598ebc2
mcbtr <mcb.gitlab@gmail.com>
2024-02-23 11:02:03 +0100

    Se consigue realizar el primer punto

--- functions/index.js
+++ functions/index.js
@@ -5,24 +5,20 @@ const logger = require("firebase-functions/logger");
 
 exports.insertElement = onRequest(async (request, response) => {
   try {
-    // Obtén los datos de la solicitud HTTP
-    const data = request.body; // Asumiendo que los datos se envían en el cuerpo de la solicitud POST
+    const data = request.body;
 
-    // Inserta los datos en la raíz de la base de datos
-    const result = await admin.database().ref().push(data);
+    const usuariosCollection = admin.firestore().collection("Usuarios");
 
-    // Obtiene el ID del elemento insertado
-    const elementId = result.key;
+    const result = await usuariosCollection.add(data);
 
-    // Log para registrar en Firebase
+    const elementId = result.id;
+
     logger.info(`Elemento con ID ${elementId} fue insertado correctamente.`, { structuredData: true });
 
-    // Envía una respuesta al cliente
     response.status(200).send(`Elemento con ID ${elementId} fue insertado correctamente.`);
   } catch (error) {
     console.error('Error al insertar el elemento:', error);
 
-    // Log para registrar en Firebase
     logger.error('Error al insertar el elemento en la base de datos.', { error: error });
 
     response.status(500).send('Error al insertar el elemento en la base de datos.');
